584fee8c6ace2e35070bc190f06c54123b2726e8
kennethsteuart <krs00046@mix.wvu.edu>
2025-03-27 21:41:41 -0400

    MULT implementation

--- MULT.c
+++ MULT.c
@@ -1,12 +1,55 @@
 #include "Instruction.h"
 
-// will do later
+
 
 void mult_reg_assm(void) {
-	state = WRONG_COMMAND;
+	if (strcmp(OP_CODE, "MULT") != 0) {
+		state = WRONG_COMMAND;
+		return;
+	}
+
+	// check par
+	if (PARAM1.type != REGISTER) {
+		state = MISSING_REG;
+		return;
+	}
+	if (PARAM2.type != REGISTER) {
+		state = MISSING_REG;
+		return;
+	}
+	if (PARAM1.value > 31) {
+		state = INVALID_REG;
+		return;
+	}
+	if (PARAM2.value > 31) {
+		state = INVALID_REG;
+		return;
+	}
+
+	// create binary
+	setBits_str(31, "000000");
+	setBits_num(25, PARAM1.value, 5); 
+	setBits_num(20, PARAM2.value, 5); 
+	setBits_str(5, "011000");
+	setBits_num(15, 0, 10);
+	
+	state = COMPLETE_ENCODE;
 }
 
 void mult_reg_bin(void) {
-	state = WRONG_COMMAND;
+	if (checkBits(31, "000000") != 0 || checkBits(5, "011000") != 0) {
+		state = WRONG_COMMAND;
+		return;
+	}
+
+	// find binary
+	uint32_t Rs = getBits(25, 5);
+	uint32_t Rt = getBits(20, 5);
+
+	setOp("MULT");
+	setParam(1, REGISTER, Rs);
+	setParam(2, REGISTER, Rt); 
+
+	state = COMPLETE_DECODE;
 }
 
