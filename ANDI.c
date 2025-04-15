38d1c0bd8b91c32ce6ba62eb614d479580ccf4d7
Killian <kpm00013@mix.wvu.edu>
2025-04-15 17:31:43 -0400

    extra comments for presentation

--- ANDI.c
+++ ANDI.c
@@ -3,64 +3,71 @@
 void andi_immd_assm(void) {
 	// Check for "ANDI"
 	if (strcmp(OP_CODE, "ANDI") != 0) {
+		//Incorrect opcode, it doesnt match 
 		state = WRONG_COMMAND;
 		return;
 	}
 
 	// check params
+	//first one shoudl eb a destination register
 	if (PARAM1.type != REGISTER) {
 		state = MISSING_REG;
 		return;
 	}
+	//this should also be a register but a source register
 	if (PARAM2.type != REGISTER) {
 		state = MISSING_REG;
 		return;
 	}
+	//The third parameter should be the immediate to be used
 	if (PARAM3.type != IMMEDIATE) {
 		state = INVALID_PARAM;
 		return;
 	}
-
+	//Check the values of the paramet
+	//First one should be 31 or less
 	if (PARAM1.value > 31) {
 		state = INVALID_REG;
 		return;
 	}
+	//This one should also be 31 or less
 	if (PARAM2.value > 31) {
 		state = INVALID_REG;
 		return;
 	}
+	//The immediate should be a 16 bit value of 0xFFFF or less
 	if (PARAM3.value > 0xFFFF) {
 		state = INVALID_IMMED;
 		return;
 	}
 
-	// combine binary
+	// putting the binary together
 	setBits_str(31, "001100"); // Set opcode
 	setBits_num(20, PARAM1.value, 5); //Set destination
 	setBits_num(25, PARAM2.value, 5); // Set source
 	setBits_num(15, PARAM3.value, 16); // Set immediate
-
+	//tell the system that the encoding is complete
 	state = COMPLETE_ENCODE;
 }
 
 void andi_immd_bin(void) {
-	//Verify opcode
+	//Verify opcode matches
 	if (checkBits(31, "001100") != 0 ) {
 		state = WRONG_COMMAND;
 		return;
 	}
 
 	// get binary values
-	uint32_t Rs = getBits(25, 5);
-	uint32_t Rt = getBits(20, 5);
-	uint32_t imm16 = getBits(15, 16);
+	uint32_t Rs = getBits(25, 5);//get source register
+	uint32_t Rt = getBits(20, 5);//get destination register
+	uint32_t imm16 = getBits(15, 16);//get immediate value
 
-	// set op
-	setOp("ANDI");
-	setParam(1, REGISTER, Rt); 
-	setParam(2, REGISTER, Rs); 
-	setParam(3, IMMEDIATE, imm16);
-	
+	// set instruction values
+	setOp("ANDI");//set the operation name
+	setParam(1, REGISTER, Rt); //set destination register
+	setParam(2, REGISTER, Rs); //set source register
+	setParam(3, IMMEDIATE, imm16);//set immediate value
+	//tell the system decoding is done
 	state = COMPLETE_DECODE;
 }
 
--- DIV.c
+++ DIV.c
@@ -6,38 +6,49 @@
 */
 
 #include "Instruction.h"
-//Check opcode
+//Check opcode is "DIV"
 void div_reg_assm(void) {
 	if (strcmp(OP_CODE, "DIV") != 0) {
+		//If the op code doesn't match, this is the wrong command
 		state = WRONG_COMMAND;
 		return;
 	}
 
-	// check par
+	// check parameter types
+	//first one must be a register (Rs)
 	if (PARAM1.type != REGISTER) {
 		state = MISSING_REG;
 		return;
 	}
+	//This must also be a register (Rt)
 	if (PARAM2.type != REGISTER) {
 		state = MISSING_REG;
 		return;
 	}
+	//Check the parameter values
+	//Rs must be within valid register range(under 31)
 	if (PARAM1.value > 31) {
 		state = INVALID_REG;
 		return;
 	}
+	//Rt must also be within the valid register range
 	if (PARAM2.value > 31) {
 		state = INVALID_REG;
 		return;
 	}
 
 	// create binary
+	//Set the op code to 0
 	setBits_str(31, "000000");
+	//Set source register in the instruction(Rs)
 	setBits_num(25, PARAM1.value, 5); //BUG FIX - changed 20 to 25 so Rs is the first paramter in the machine code
+	//Set the target register (Rt)
 	setBits_num(20, PARAM2.value, 5); //BUG FIX - changed 25 to 20 so Rt is the second parameter in the machine code
+	//Set the code for DIV
 	setBits_str(5, "011010");
+	//Set the remaining bits to 0
 	setBits_num(15, 0, 10);// Padding bits
-	
+	//Tell the system it is complete!
 	state = COMPLETE_ENCODE;
 }
 
